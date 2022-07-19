7b81a7574cd83ea96982c913a1885c14615e5e0e
Michael M <melancholic_maclean@yahoo.com>
2022-07-18 22:56:08 -0400

    added feather files. updated requirements.txt

--- data_helpers.py
+++ data_helpers.py
@@ -3,10 +3,7 @@ import os
 
 
 def get_intake_outcomes_data():
-    intakes = pd.read_csv(f'{os.getcwd()}/data/full/Austin_Animal_Center_Intakes_7_17_2022.csv')
-    intakes['Date'] = pd.to_datetime(pd.to_datetime(intakes['DateTime']).dt.strftime('%Y-%m-%d'))
-
-    outcomes = pd.read_csv(f'{os.getcwd()}/data/full/Austin_Animal_Center_Outcomes_7_17_2022.csv')
-    outcomes['Date'] = pd.to_datetime(pd.to_datetime(outcomes['DateTime']).dt.strftime('%Y-%m-%d'))
+    intakes = pd.read_feather(f'{os.getcwd()}/data/full/Austin_Animal_Center_Intakes_7_17_2022.ftr')
+    outcomes = pd.read_feather(f'{os.getcwd()}/data/full/Austin_Animal_Center_Outcomes_7_17_2022.ftr')
 
     return intakes, outcomes
