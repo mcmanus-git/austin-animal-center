8bf350455617820896ac63ecbc345f8118c0d4d1
Michael M <melancholic_maclean@yahoo.com>
2022-07-18 23:38:18 -0400

    updated title

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ import dash_bootstrap_components as dbc
 
 FA = "https://use.fontawesome.com/releases/v5.12.1/css/all.css"
 FA2 = "https://use.fontawesome.com/releases/v6.0.0/css/all.css"
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX, FA, FA2])
+app = dash.Dash(__name__,
+                suppress_callback_exceptions=True,
+                external_stylesheets=[dbc.themes.LUX, FA, FA2],
+                title='AAC Insights')
